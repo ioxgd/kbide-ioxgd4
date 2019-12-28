bf2c9faf058e9895ed4ea0c5fa4e809cbe0af65c
Max <max30012540@hotmail.com>
2019-12-28 09:29:44 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: "ioxgd4",
+  platform: "arduino-avr",
+  title: "IOXGD4",
+  description: "IOXGD4 HMI development platform",
+  author: "Sonthaya Nongnuch",
+  website: "http://www.ioxgd.com",
+  email: "max30012540@hotmail.com",
+  git: "https://github.com/ioxgd/kbide-ioxgd4",
+  image: "/static/display.jpg",
+  version: "1.1.0"
+};
