3896f49f2aac49dd0667c7586024f03103c1c502
Max <max30012540@hotmail.com>
2019-12-31 21:37:13 +0700

    เพิ่มตรวจเช็คการอัพเดทของไฟล์ออกแบบอัตโนมัติ

--- block/generators_object.js
+++ block/generators_object.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require('path');
 const { promisify } = require('util');
+const crypto = require('crypto');
 const { dialog } = require("electron").remote;
 const ioxgd_codegen = require("../ioxgd/codegen");
 
@@ -12,7 +13,9 @@ const GB = Vue.prototype.$global;
 
 GB.ioxgd = {
     blocks: [],
-    component: []
+    component: [],
+    designfile: "",
+    designfile_md5: ""
 }
 
 let checkHasObject = (object_name) => {
@@ -27,135 +30,216 @@ let objectNameFirst = (object_name) => {
     }
 };
 
-module.exports = function(Blockly) {
-    Blockly.getMainWorkspace().registerButtonCallback("importDesignerProject", async () => {
-        let result = await dialog.showOpenDialog({
-            properties: [
-                'openFile'
-            ],
-            filters: [{ 
-                name: 'GD', 
-                extensions: ['gd'] 
-            }]
-        });
+let md5file = (file) => new Promise((resolve, reject) => {
+    let md5sum = crypto.createHash('md5');
+    try {
+      let s = fs.ReadStream(file);
+
+      s.on('data', function (data) {
+        md5sum.update(data)
+      });
+
+      s.on('end', function () {
+        return resolve(md5sum.digest('hex'));
+      });
+    } catch (error) {
+      return reject(error);
+    }
+});
+
+// Set timer for check file update
+GB.ioxgd.check_designfile_update = async () => {
+    let hash = await md5file(GB.ioxgd.designfile);
+    if (hash !== GB.ioxgd.designfile_md5 && GB.ioxgd.designfile_md5.length > 0) {
+        await (new Promise((resolve, reject) => {
+            // Ask user update file
+            Vue.prototype.$dialog.confirm({
+                text: 'Design file are update. Are you want to update Blocks ?',
+                title: 'IOXGD Notify',
+                actions: {
+                    false: {
+                        text: 'Next time',
+                        handle: resolve
+                    },
+                    true: {
+                        color: 'green',
+                        text: 'Update Now',
+                        handle: () => {
+                            GB.$emit("ioxgd-reload-design-file");
+                            resolve();
+                        }
+                    }
+                }
+            });
+        }));
+    }
+    GB.ioxgd.designfile_md5 = hash;
+
+    // Timer via setTimeout, pulling every 1s sec
+    GB.ioxgd.check_designfile_update_timer = setTimeout(GB.ioxgd.check_designfile_update, 1000);
+};
+
+GB.$on('ioxgd-reload-design-file', async () => {
+    // Stop timer
+    clearTimeout(GB.ioxgd.check_designfile_update_timer);
+    GB.ioxgd.designfile_md5 = "";
+
+    let fileContent = fs.readFileSync(GB.ioxgd.designfile);
+    let fileObject = JSON.parse(fileContent);
     
-        if (result == undefined) {
-            return;
-        }
+    GB.ioxgd.component = [];
+    let components = fileObject.page[0].component;
+    for (let [name, component] of Object.entries(components)) {
+        GB.ioxgd.component.push(component);
+    }
     
-        let OpenfilePath = result[0];
+    GB.ioxgd.blocks = new DOMParser().parseFromString(`
+<xml>
+  <label text="Any" web-class="headline"></label>
+  <block type="object_show"></block>
+  <block type="object_hide"></block>
+${checkHasObject('Label') ? `
+  <sep gap="32"></sep>
+
+  <label text="Label" web-class="headline"></label>
+  <block type="object_set_text">
+    <field name="object">${objectNameFirst('Label')}</field>
+    <value name="text">
+        <shadow type="basic_string">
+            <field name="VALUE">Hello, IOXGD !</field>
+        </shadow>
+    </value>
+  </block>
+  ` : ''}
+  ${checkHasObject('Button') ? `
+  <sep gap="32"></sep>
+
+  <label text="Button" web-class="headline"></label>
+  <block type="object_on_click">
+    <field name="object">${objectNameFirst('Button')}</field>
+  </block>
+  <block type="object_set_text">
+    <field name="object">${objectNameFirst('Button')}</field>
+    <value name="text">
+        <shadow type="basic_string">
+            <field name="VALUE">Hello, IOXGD !</field>
+        </shadow>
+    </value>
+  </block>
+  ` : ''}
+  ${checkHasObject('LED') ? `
+  <sep gap="32"></sep>
+
+  <label text="LED" web-class="headline"></label>
+  <block type="object_led_on">
+    <field name="object">${objectNameFirst('LED')}</field>
+  </block>
+  <block type="object_led_off">
+    <field name="object">${objectNameFirst('LED')}</field>
+  </block>
+  <block type="object_led_set_bright">
+    <field name="object">${objectNameFirst('LED')}</field>
+    <value name="brightness">
+      <shadow type="math_number">
+        <field name="NUM">190</field>
+      </shadow>
+    </value>
+  </block>
+  ` : ''}
+  ${checkHasObject('Chart') ? `
+  <sep gap="32"></sep>
+
+  <label text="Chart" web-class="headline"></label>
+  ` : ''}
+  ${checkHasObject('Switch') ? `
+  <sep gap="32"></sep>
+
+  <label text="Switch" web-class="headline"></label>
+  <block type="object_on_value_changed">
+    <field name="object">${objectNameFirst('Switch')}</field>
+  </block>
+
+  <sep gap="32"></sep>
+  ` : ''}
+  ${checkHasObject('Slider') ? `
+  <label text="Slider" web-class="headline"></label>
+  <block type="object_on_value_changed">
+    <field name="object">${objectNameFirst('Slider')}</field>
+  </block>
+  <block type="object_slider_get_value">
+    <field name="object">${objectNameFirst('Slider')}</field>
+  </block>
+  <block type="object_slider_set_value">
+    <field name="object">${objectNameFirst('Slider')}</field>
+    <value name="value">
+      <shadow type="math_number">
+        <field name="NUM">50</field>
+      </shadow>
+    </value>
+  </block>
+  ` : ''}
+</xml>
+    `, "text/xml").firstChild.children;
     
-        let fileContent = fs.readFileSync(OpenfilePath);
-        let fileObject = JSON.parse(fileContent);
-        
-        GB.ioxgd.component = [];
-        let components = fileObject.page[0].component;
-        for (let [name, component] of Object.entries(components)) {
-            GB.ioxgd.component.push(component);
-        }
-        
-        GB.ioxgd.blocks = new DOMParser().parseFromString(`
-    <xml>
-      <label text="Any" web-class="headline"></label>
-      <block type="object_show"></block>
-      <block type="object_hide"></block>
-    ${checkHasObject('Label') ? `
-      <sep gap="32"></sep>
+    let allBlocks = GB.editor.workspace.getAllBlocks();
+    if (!allBlocks.find(x => x.type === 'object_load_page')) {
+        // Add load page block to workspace
+        var loadPageBlock = GB.editor.workspace.newBlock('object_load_page');
+        loadPageBlock.initSvg();
+        loadPageBlock.render();
+    }
+
+    // Update toolbox
+    GB.editor.workspace.getToolbox().refreshSelection();
     
-      <label text="Label" web-class="headline"></label>
-      <block type="object_set_text">
-        <field name="object">${objectNameFirst('Label')}</field>
-        <value name="text">
-            <shadow type="basic_string">
-                <field name="VALUE">Hello, IOXGD !</field>
-            </shadow>
-        </value>
-      </block>
-      ` : ''}
-      ${checkHasObject('Button') ? `
-      <sep gap="32"></sep>
-    
-      <label text="Button" web-class="headline"></label>
-      <block type="object_on_click">
-        <field name="object">${objectNameFirst('Button')}</field>
-      </block>
-      <block type="object_set_text">
-        <field name="object">${objectNameFirst('Button')}</field>
-        <value name="text">
-            <shadow type="basic_string">
-                <field name="VALUE">Hello, IOXGD !</field>
-            </shadow>
-        </value>
-      </block>
-      ` : ''}
-      ${checkHasObject('LED') ? `
-      <sep gap="32"></sep>
-    
-      <label text="LED" web-class="headline"></label>
-      <block type="object_led_on">
-        <field name="object">${objectNameFirst('LED')}</field>
-      </block>
-      <block type="object_led_off">
-        <field name="object">${objectNameFirst('LED')}</field>
-      </block>
-      <block type="object_led_set_bright">
-        <field name="object">${objectNameFirst('LED')}</field>
-        <value name="brightness">
-          <shadow type="math_number">
-            <field name="NUM">190</field>
-          </shadow>
-        </value>
-      </block>
-      ` : ''}
-      ${checkHasObject('Chart') ? `
-      <sep gap="32"></sep>
-    
-      <label text="Chart" web-class="headline"></label>
-      ` : ''}
-      ${checkHasObject('Switch') ? `
-      <sep gap="32"></sep>
-    
-      <label text="Switch" web-class="headline"></label>
-      <block type="object_on_value_changed">
-        <field name="object">${objectNameFirst('Switch')}</field>
-      </block>
-    
-      <sep gap="32"></sep>
-      ` : ''}
-      ${checkHasObject('Slider') ? `
-      <label text="Slider" web-class="headline"></label>
-      <block type="object_on_value_changed">
-        <field name="object">${objectNameFirst('Slider')}</field>
-      </block>
-      <block type="object_slider_get_value">
-        <field name="object">${objectNameFirst('Slider')}</field>
-      </block>
-      <block type="object_slider_set_value">
-        <field name="object">${objectNameFirst('Slider')}</field>
-        <value name="value">
-          <shadow type="math_number">
-            <field name="NUM">50</field>
-          </shadow>
-        </value>
-      </block>
-      ` : ''}
-    </xml>
-        `, "text/xml").firstChild.children;
-        
-        let allBlocks = Blockly.getMainWorkspace().getAllBlocks();
-        if (!allBlocks.find(x => x.type === 'object_load_page')) {
-            // Add load page block to workspace
-            var loadPageBlock = Blockly.getMainWorkspace().newBlock('object_load_page');
-            loadPageBlock.initSvg();
-            loadPageBlock.render();
-        }
-    
-        // Update toolbox
-        Blockly.getMainWorkspace().getToolbox().refreshSelection();
-        
-        // Gen code into include dir
-        ioxgd_codegen(OpenfilePath);
+    // Gen code into include dir
+    ioxgd_codegen(GB.ioxgd.designfile);
+
+    // Start timer
+    GB.ioxgd.check_designfile_update();
+});
+
+let importButton = async () => {
+    let result = await dialog.showOpenDialog({
+        properties: [
+            'openFile'
+        ],
+        filters: [{ 
+            name: 'GD', 
+            extensions: ['gd'] 
+        }]
     });
+
+    if (result == undefined) {
+        return;
+    }
+
+    GB.ioxgd.designfile = result[0];
+
+    GB.$emit("ioxgd-reload-design-file");
+}
+
+// when page loaded
+GB.$once('board-package-loaded', () => {
+    // Ask user import file
+    Vue.prototype.$dialog.confirm({
+        text: 'Please import a design file for program GUI via Blocks.',
+        title: 'IOXGD Notify',
+        actions: {
+            false: 'Next time',
+            true: {
+                color: 'green',
+                text: 'Import Now',
+                handle: importButton
+            }
+        }
+    });
+});
+
+module.exports = function(Blockly) {
+    
+
+    Blockly.getMainWorkspace().registerButtonCallback("importDesignerProject", importButton);
     
     Blockly.getMainWorkspace().registerToolboxCategoryCallback('getObjectBlocks', () => {
         const Blockly = GB.editor.Blockly;
@@ -180,6 +264,9 @@ module.exports = function(Blockly) {
     for (let file of oldFiles) {
         fs.unlink(file, () => console.info(`delete ${file}`));
     }
+
+    
+    
 
     // ----- Blocks -----  //
     let defineObject = (name) => `#VARIABLEextern lv_obj_t *${name};#END`;
